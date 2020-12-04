a9655e4316e6b9126606bddefd122990b2eda7af
Abhishek Chakraborty <abhishek@Datas-MacBook-Pro-2.local>
2020-12-04 13:10:48 -0500

    openshift: reverted run through port 8080 and exposing again through 8080

--- flask_app/run.py
+++ flask_app/run.py
@@ -3,4 +3,4 @@ from gevent.pywsgi import WSGIServer
 
 if __name__ == '__main__':
     #app.run(debug=True, host='0.0.0.0', port=5000)
-    WSGIServer(('0.0.0.0', 5000), app).serve_forever()
+    WSGIServer(('0.0.0.0', 8080), app).serve_forever()
